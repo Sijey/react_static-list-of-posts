11706d2c80c141515837310288ccc00ee8b4df07
Sijey <Popov040391@gmail.com>
2019-10-27 11:49:37 +0200

    Updates

---
+++ precache-manifest.58d55634efabb43cfafb47682f544943.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6b835500955c761f7bbc4ff2332b241",
+    "url": "/react_static-list-of-posts/index.html"
+  },
+  {
+    "revision": "a5ea43337c6406a9d4e7",
+    "url": "/react_static-list-of-posts/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "97a6fe4b3321c695088c",
+    "url": "/react_static-list-of-posts/static/js/2.ee13c243.chunk.js"
+  },
+  {
+    "revision": "a5ea43337c6406a9d4e7",
+    "url": "/react_static-list-of-posts/static/js/main.46a1ced8.chunk.js"
+  },
+  {
+    "revision": "3fdea9cdc8e2a1e9a77e",
+    "url": "/react_static-list-of-posts/static/js/runtime~main.c48bb6e1.js"
+  }
+]);
